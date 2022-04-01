85c049f38758c35397f1b51125a070eccebd796a
mnaofficial <mna.official92@gmail.com>
2022-04-02 00:05:00 +0600

    useCart hook

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 import "./Orders.css"
 
 const Orders = () => {
-    const [products, setProducts]=useProducts();
+    const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products)
     return (
         <div>
             <h3>This is Orders: {products.length}</h3>
+            <p>Cart has: {cart.length}</p>
         </div>
     );
 };
