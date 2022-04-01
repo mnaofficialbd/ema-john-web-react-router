80da5138bfe32e7b7686aa160ef67190a7968be0
mnaofficial <mna.official92@gmail.com>
2022-04-02 01:16:18 +0600

    order cart

--- src/Components/ReviewItem/ReviewItem.js
+++ src/Components/ReviewItem/ReviewItem.js
@@ -2,10 +2,23 @@ import React from 'react';
 import './ReviewItem.css'
 
 const ReviewItem = (props) => {
-    const { name, price, shipping, quantity } = props.product;
+    const { name, img, price, shipping, quantity } = props.product;
     return (
-        <div>
-            <h3>This is ReviewItem: {name}</h3>
+        <div className='review-item'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="review-item-details-container">
+                <div className="review-item-details">
+                    <p className="product-name" title={name}>{name.length > 20 ? name.slice(0, 20) + '...' : name}</p>
+                    <p>Price: <span className='orange-color'>${price}</span></p>
+                    <p><small>Shipping: <span className='orange-color'>${shipping}</span></small></p>
+                    <p><small>Quantity: <span className='orange-color'>{quantity}</span></small></p>
+                </div>
+                <div className="delete-container">
+                    <button>Delete</button>
+                </div>
+            </div>
         </div>
     );
 };
