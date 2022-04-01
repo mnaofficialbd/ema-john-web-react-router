d57b7909522a4ee08cc7318f9185d27160b480ff
mnaofficial <mna.official92@gmail.com>
2022-04-01 22:17:36 +0600

    custom hook

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 import "./Orders.css"
 
 const Orders = () => {
+    const [products, setProducts]=useProducts();
     return (
         <div>
-            <h3>This is Orders</h3>
+            <h3>This is Orders: {products.length}</h3>
         </div>
     );
 };
