800f1a66a21ea60822517e8fce2a717726afd07f
mnaofficial <mna.official92@gmail.com>
2022-04-01 21:19:14 +0600

    about route

---
+++ src/Components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css'
+
+const About = () => {
+    return (
+        <div>
+            <h4>This is About</h4>
+        </div>
+    );
+};
+
+export default About;
